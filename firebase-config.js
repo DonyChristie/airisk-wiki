34719b8d3d7d0313fcd87ab54b99ef1c4e2007ef
DonyChristie <donychristie@gmail.com>
2025-11-15 22:05:13 -0800

    pages editable

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyD6QI-nODBLSQDXxvMDzoCVh73kHoIpm_o",
+  authDomain: "ai-risk-wiki.firebaseapp.com",
+  projectId: "ai-risk-wiki",
+  storageBucket: "ai-risk-wiki.firebasestorage.app",
+  messagingSenderId: "244725504960",
+  appId: "1:244725504960:web:dbd219e66932e8cb76c625",
+  measurementId: "G-E66WG8MCRT"
+};
